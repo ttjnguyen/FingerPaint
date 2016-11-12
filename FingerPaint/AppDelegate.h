f5267a0ea0f5dc31a66777aeccc76bee059556d3
ttjnguyen <ttjnguyen6@gmail.com>
2016-11-11 17:08:24 -0800

    Initial Commit

---
+++ FingerPaint/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FingerPaint
+//
+//  Created by Jenny Nguyen on 2016-11-11.
+//  Copyright © 2016 LightHouseLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
